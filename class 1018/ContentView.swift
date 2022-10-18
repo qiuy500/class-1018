84b9eff467cc2dbc4b811266e629704df14c524a
Dujiashan0711 <shandu7652@gmail.com>
2022-10-18 22:23:28 +0800

    Initial Commit

---
+++ class 1018/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  class 1018
+//
+//  Created by 暨大附中２ on 2022/10/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ class 1018/class_1018App.swift
@@ -0,0 +1,17 @@
+//
+//  class_1018App.swift
+//  class 1018
+//
+//  Created by 暨大附中２ on 2022/10/18.
+//
+
+import SwiftUI
+
+@main
+struct class_1018App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
